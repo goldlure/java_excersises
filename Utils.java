05b72bcddbcb2eb67abc12c8fb9e7ca610ec5d53
goldlure <goldlure@gmail.com>
2019-09-19 21:36:55 +0300

    added Utils to printArray

---
+++ Utils.java
@@ -0,0 +1,8 @@
+public class Utils{
+    public static <T> void printArray(T[] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+    }
+}
