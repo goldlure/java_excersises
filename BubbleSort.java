4f904bb4c048519b48d229c36ff50a67c0e5f7fa
goldlure <goldlure@gmail.com>
2019-09-20 08:54:38 +0300

    test bubble

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,6 +1,22 @@
 public class BubbleSort extends Sorter implements Sortable{
     @Override
     public <T extends Comparable<T>> void sortAscending(T[] values){
+        sort01(values,0,1);
+    }
+    @Override
+    public <T extends Comparable<T>> void sortDescending(T[] values){
+        sort01(values,1,0);
+    }
+
+    private <T extends Comparable<T>> void sort01(T[] values, int first, int second){
+        int length = values.length;
         
+        for(int i = 1; i < length; i++){
+            for (j = 0; j < length-i; j++){
+                if (values[j + first].compareTo(values[second]) > 0){
+                    swap(values, j+first, j+second);
+                }
+            }
+        }
     }
 }
--- SorterApp.java
+++ SorterApp.java
@@ -1,10 +1,19 @@
 public class SorterApp {
     protected void run() {
-        sort(new Integer[] {});
+        sort(new Integer[] { 1, 2, 3, 4, 5 });
+        sort(new Integer[] { 3, 1, 5, 4, 2 });
+        sort(new Integer[] { 5, 4, 3, 2, 1 });
 
+        System.out.println();
+
+        sort(new String[] { "a", "b", "c", "d", "e" });
+        sort(new String[] { "c", "a", "e", "d", "b" });
+        sort(new String[] { "e", "d", "c", "b", "a" });
     }
     private <T extends Comparable<T>> void sort(T[] values) {
         Sortable sorter = new BubbleSort();
-        
+        sorter.sortAscending(values);
+        // sorter.sortDescending(values);
+        Utils.printArray(values);
     }
 }
