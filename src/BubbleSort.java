7ff540e80440a05e65a5441dda0b7c0d64a488b1
goldlure <goldlure@gmail.com>
2019-09-20 12:23:09 +0300

    sort user type Student

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,40 +1,32 @@
 class BubbleSort extends Sorter implements Sortable{
     @Override
     public <T extends Comparable<T>> void sortAscending(T[] values){
-        sort01(values,"asc");
+        sort(values,"asc");
     }
     @Override
     public <T extends Comparable<T>> void sortDescending(T[] values){
-        sort01(values,"desc");
+        sort(values,"desc");
     }
 
-    private <T extends Comparable<T>> void sort01(T[] values, String order){
+    private <T extends Comparable<T>> void sort(T[] values, String order){
         int length = values.length;
         boolean notSwapped;
-        // for(int i = 1; i < length; i++){
-        //     for (int j = 0; j < length-i; j++){
-        //         if (values[j + first].compareTo(values[second]) > 0){
-        //             swap(values, j+first, j+second);
-        //         }
-        //     }
-        // }
         for (int i=0; i<length; i++) {
             notSwapped = true;
             for (int j = 1; j<length-i; j++){
                 if (values[j-1].compareTo(values[j])>0 && order.equals("asc")){
                     swap(values, j-1, j);
                     notSwapped = false; 
-                    System.out.println("unsorted up"); 
+                    //System.out.println("unsorted up"); 
                 } 
                 else if (values[j-1].compareTo(values[j])<0 && order.equals("desc")){
                     swap(values, j-1, j);
                     notSwapped = false;  
-                    System.out.println("unsorted down");
+                    //System.out.println("unsorted down");
                 } 
-                
             }
             if (notSwapped){
-                System.out.println("sorted");
+                //System.out.println("sorted");
                 break;
             }
         }
--- src/SorterApp.java
+++ src/SorterApp.java
@@ -9,6 +9,13 @@ class SorterApp {
         sort(new String[] { "a", "b", "c", "d", "e" });
         sort(new String[] { "c", "a", "e", "d", "b" });
         sort(new String[] { "e", "d", "c", "b", "a" });
+
+        Student s1 = new Student(18, "s1", 45);
+        Student s2 = new Student(19, "s2", 40);
+        Student s3 = new Student(17, "s3", 55);
+
+        sort(new Student[] {s1,s2,s3});
+        
     }
     private <T extends Comparable<T>> void sort(T[] values) {
         Sortable sorter = new BubbleSort();
