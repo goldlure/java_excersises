cd65be4eab72004632b145429cbf97ba913d7132
goldlure <goldlure@gmail.com>
2019-09-20 10:39:51 +0300

    new structure

--- src/Utils.java
+++ src/Utils.java
@@ -1,7 +1,7 @@
 public class Utils{
     public static <T> void printArray(T[] arr){
         for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
     }
---
+++ src/test1.java
@@ -0,0 +1,43 @@
+public class test1 {   
+   public static void main(String[] args) {
+      {
+      // Stack<Integer> stack  = new Stack(10);
+      // System.out.println(stack.getTop());
+      // stack.Push(90);
+      // System.out.println(stack.Peek());
+      // stack.Push(91);
+      // System.out.println(stack.Peek());
+      // stack.Push(92);
+      // System.out.println(stack.Peek());
+      // stack.Pop();
+      // System.out.println(stack.Peek());
+      // stack.Pop();
+      // System.out.println(stack.Peek());
+      // System.out.println("Hello, world!");
+      // Stack<String> stackStr  = new Stack(10);
+      // System.out.println(stackStr.getTop());
+      // stackStr.Push("90 string");
+      // System.out.println(stackStr.Peek());
+      // stackStr.Push("91 string");
+      // System.out.println(stackStr.Peek());
+      // stackStr.Push("92 string");
+      // System.out.println(stackStr.Peek());
+      // stackStr.Pop();
+      // System.out.println(stackStr.Peek());
+      // stackStr.Pop();
+      // System.out.println(stackStr.Peek());
+      // stackStr.Pop();
+      // System.out.println(stackStr.Peek());
+      }
+      
+      int arr[] ={3,60,35,2,45,320,5};  
+      Sort.bubbleSort(arr);
+      System.out.println("Array After Bubble Sort");  
+      for(int i=0; i < arr.length; i++){  
+         System.out.print(arr[i] + " ");  
+      }
+      System.out.println();
+      SorterApp app = new SorterApp();
+      app.run();  
+   }
+}
