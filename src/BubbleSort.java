2aae8251b741af761e44f4e636cf97746571a4a4
goldlure <goldlure@gmail.com>
2019-09-20 11:50:09 +0300

    add asc and desc

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,21 +1,41 @@
 class BubbleSort extends Sorter implements Sortable{
     @Override
     public <T extends Comparable<T>> void sortAscending(T[] values){
-        sort01(values,0,1);
+        sort01(values,"asc");
     }
     @Override
     public <T extends Comparable<T>> void sortDescending(T[] values){
-        sort01(values,1,0);
+        sort01(values,"desc");
     }
 
-    private <T extends Comparable<T>> void sort01(T[] values, int first, int second){
+    private <T extends Comparable<T>> void sort01(T[] values, String order){
         int length = values.length;
-        
-        for(int i = 1; i < length; i++){
-            for (int j = 0; j < length-i; j++){
-                if (values[j + first].compareTo(values[second]) > 0){
-                    swap(values, j+first, j+second);
-                }
+        boolean notSwapped;
+        // for(int i = 1; i < length; i++){
+        //     for (int j = 0; j < length-i; j++){
+        //         if (values[j + first].compareTo(values[second]) > 0){
+        //             swap(values, j+first, j+second);
+        //         }
+        //     }
+        // }
+        for (int i=0; i<length; i++) {
+            notSwapped = true;
+            for (int j = 1; j<length-i; j++){
+                if (values[j-1].compareTo(values[j])>0 && order.equals("asc")){
+                    swap(values, j-1, j);
+                    notSwapped = false; 
+                    System.out.println("unsorted up"); 
+                } 
+                else if (values[j-1].compareTo(values[j])<0 && order.equals("desc")){
+                    swap(values, j-1, j);
+                    notSwapped = false;  
+                    System.out.println("unsorted down");
+                } 
+                
+            }
+            if (notSwapped){
+                System.out.println("sorted");
+                break;
             }
         }
     }
--- src/SorterApp.java
+++ src/SorterApp.java
@@ -1,7 +1,7 @@
 class SorterApp {
     protected void run() {
         sort(new Integer[] { 1, 2, 3, 4, 5 });
-        sort(new Integer[] { 3, 1, 5, 4, 2 });
+        sort(new Integer[] { 7, 0, 4, 444, 67 });
         sort(new Integer[] { 5, 4, 3, 2, 1 });
 
         System.out.println();
@@ -13,7 +13,8 @@ class SorterApp {
     private <T extends Comparable<T>> void sort(T[] values) {
         Sortable sorter = new BubbleSort();
         sorter.sortAscending(values);
-        // sorter.sortDescending(values);
+        Utils.printArray(values);
+        sorter.sortDescending(values);
         Utils.printArray(values);
     }
 }
