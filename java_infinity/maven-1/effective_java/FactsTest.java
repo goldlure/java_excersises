05bf3905d4c8b703a3a680e0fda3b418501ec3a5
goldlure <goldlure@gmail.com>
2019-11-08 13:22:17 +0300

    stages all

--- java_infinity/maven-1/effective_java/FactsTest.java
+++
@@ -1,8 +0,0 @@
-import inna.effective_java.NutritionFacts.*;
-public class FactsTest {
-    public static void main(String[] args) {
-        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8).calories(100).sodium(35).carbohydrate(27).build();
-        cocaCola.print();
-        cocaCola.toString();
-    }
-}
